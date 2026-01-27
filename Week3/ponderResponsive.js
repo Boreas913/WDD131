149171b5b66116f3689d1f390aed9b824e9e3e00
Aurora Smith <Auroracsmith913@gmail.com>
2026-01-27 15:59:12 -0700

    resoponsiv menu

--- Week3/ponderResponsive.js
+++ Week3/ponderResponsive.js
@@ -1,6 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn');
 const nav = document.querySelector('nav');
+let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
+  if (!menuOpen){
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else{
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
   nav.classList.toggle('open');
 });

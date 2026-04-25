036a02ec1616b9c796a8c444fdd2802a251d89d2
Aurora Smith <Auroracsmith913@gmail.com>
2026-04-25 13:26:12 -0600

    done

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,3 +1,42 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('themeSwitch')
+const girlImage = document.querySelector('.girlLookingUp');
+const angelImage = document.querySelector('.angelOnCloud');
+const myImage = document.querySelector('.pictureOfMe');
+const boyImage = document.querySelector('.boyOnPillar');
+const containerImage = document.querySelector('.container_image');
+const containerImage2 = document.querySelector('.container_solid');
+
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode','active')
+    if(girlImage) girlImage.src = 'devilPeekingOverWall.png';
+    if(angelImage) angelImage.src = 'devilPlayingViolin.png';
+    if(myImage) myImage.src = 'FaceWblueBackground.jpg';
+    if(boyImage) boyImage.src = 'devilPlayingViolin.png';
+    if(containerImage) containerImage.src = '2970447842_2615c09472_k.jpg';
+    if(containerImage2) containerImage2.src = '2970447842_2615c09472_k.jpg';
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.removeItem('darkmode');
+    if(girlImage) girlImage.src = 'GirlLookingUp.png';
+    if(angelImage) angelImage.src = 'AngelOnCloud.png';
+    if(myImage) myImage.src = 'FaceWorangeBackground.jpg';
+    if(boyImage) boyImage.src = 'angelBoyOnPillar.png';
+    if(containerImage) containerImage.src = 'https://images.pexels.com/photos/6479557/pexels-photo-6479557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+    if(containerImage2) containerImage2.src = 'https://images.pexels.com/photos/6479557/pexels-photo-6479557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
 import { searchProjects } from './search.js';
 import { projects } from './projects.js';
 
@@ -46,3 +85,9 @@ function projectsTemplate(project) {
 }
 
 renderProjects(projects);
+
+
+
+
+//Make sure all Javascript works
+//implement this portfolio to be sent out to job applications
